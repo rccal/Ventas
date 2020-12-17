aad8fa950b096bf9bd54c8589bb5f09488814d7a
ridolf-desing <amoyach971@gmail.com>
2020-12-17 08:40:08 -0500

    Actualizado

--- Controladores/CRUD.php
+++ Controladores/CRUD.php
@@ -4,8 +4,6 @@ include_once 'conexion.php';
 	
 class Acciones 
 {
-
-	
 	public function Mostrar(){
 		$date=new Conexion();
 		$conexion=$date->Conectar();
@@ -14,47 +12,7 @@ class Acciones
         $resultado = $conexion->prepare($consulta);
         $resultado->execute();
         $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
-        echo' <div class="container">
-              <div align="center">
-              <h2 class="text-center"></h2>
-              <div class="row">';
-            foreach($data as $item){ 
-            	echo '<div class="col-4">
-                <form action="">
-                    <div id="imgPro" class="col-lg-4 col-md-6 col-sm-12"> 
-                        <p>Nombre:'.$item['id'] .'</p> 
-                        <p>Detalle:'.$item['Nombre'].'</p> 
-                        <p>Precio: $'.$item['Precio'] .'</p> 
-                        <input type="submit" class="btn btn-dark" value="Agregar">
-                    </div>
-
-                </form>
-            </div>';
-            }
-            echo '
-
-        </div>
-    </div>
-</div>
-
-
-<style>
-    .col-lg-4,
-    .col-md-6,
-    .col-sm-12 {
-        margin: 0;
-        padding: 25px;
-    }
-
-    #imgPro img {
-        width: 150%;
-        height: 150px;
-    }
-
-    #imgPro img:hover {
-        border: 3px solid rgb(255, 255, 255);
-    }
-</style>';
+        return $data;
 	}
 
 
--- Vistas/Productos.php
+++ Vistas/Productos.php
@@ -1,5 +1,44 @@
 <?php 
-    include_once '../Controladores/CRUD.php'; 
+    include_once '../Controladores/CRUD.php';
+    include_once '../Vistas/Layout.php'; 
     $Productos=new Acciones();
     $Mostrar=$Productos-> Mostrar();
+    
  ?>
+  <div class="container">
+              <div align="center">
+              <h2 class="text-center"></h2>
+              <div class="row">;
+            <?php foreach($Mostrar as $item){ ?>
+            	<div class="col-4">
+                <form action="">
+                    <div id="imgPro" class="col-lg-4 col-md-6 col-sm-12"> 
+                        <p>Nombre:<?php echo $item['id']?> </p> 
+                        <p>Detalle: <?php echo $item['Nombre']?> </p> 
+                        <p>Precio: $<?php echo $item['Precio'] ?> </p> 
+                        <input type="submit" class="btn btn-dark" value="Agregar">
+                    </div>
+
+                </form>
+            </div>
+            <?php } ?>
+            </div>
+        </div>
+    </div>
+            <style>
+                .col-lg-4,
+                .col-md-6,
+                .col-sm-12 {
+                margin: 0;
+                padding: 25px;
+            }
+
+            #imgPro img {
+                width: 150%;
+                height: 150px;
+            }
+
+            #imgPro img:hover {
+                border: 3px solid rgb(255, 255, 255);
+            }
+            </style>';
