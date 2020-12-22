5d955c49ab89f3fba1813637a153f411e40d9032
ridolf-desing <amoyach971@gmail.com>
2020-12-22 08:14:58 -0500

    date

--- Vistas/Layout.php
+++ Vistas/Layout.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <title>Grupo1</title>
+    <title>Tienda</title>
 </head>
 <body>
     
